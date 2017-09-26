4537cc287f1d30e850ba4bb46c95f6163a8e8fe9
Brian Lee <brianjleeofcl@gmail.com>
2017-09-26 16:32:42 -0700

    Add app class to print result

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,15 +11,24 @@ public class App {
 
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
-        Query query = em.createQuery("from User");
-        List results = query.getResultList();
-        for (User user: (List<User>) results) {
-            System.out.printf("#%d: %s %s, tel: %s%n",user.getId(), user.getFirstName(), user.getLastName(), user.getPhoneNumber());
-        }
+        if (args.length == 0) {
+            Query query = em.createQuery("from User");
+            List results = query.getResultList();
+            for (User user : (List<User>) results) {
+                System.out.printf("#%d: %s %s, tel: %s%n", user.getId(), user.getFirstName(), user.getLastName(), user.getPhoneNumber());
+            }
 
-        em.close();
-        if (!em.isOpen()) {
-            System.exit(0);
+            em.close();
+            if (!em.isOpen()) {
+                System.exit(0);
+            }
+        }
+        else if (args.length == 3) {
+            User user = new User();
+            user.setFirstName(args[0]);
+            user.setLastName(args[1]);
+            user.setPhoneNumber(args[2]);
+            em.persist(user);
         }
     }
 }
